a1bf21433b78c2d2c2c01c8c126f03ba268bf7b2
bestroom449k <bestroom449@gmail.com>
2026-09-01 17:15:52 +0900

    1.개발 환경 구축. JavaScript 실행

---
+++ server.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+console.log(process.version);
+console.log(process.cwd());
+
+
+// 파일 출력 하자
+
+const fs = require('fs'); //file system 라이브러리
+console.log(fs.readFileSync(__filename, "utf8"));
